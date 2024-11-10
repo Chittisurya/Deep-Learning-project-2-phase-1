7bc472e8bb6f3cf60d2a12e5e433a1c2a18ad27e
Chittisurya <145158399+Chittisurya@users.noreply.github.com>
2024-11-11 01:25:10 +0530

    Update coco_validation.py

--- coco_validation.py
+++ coco_validation.py
@@ -6,7 +6,6 @@ from retinanet import model
 from retinanet.dataloader import CocoDataset, Resizer, Normalizer
 from retinanet import coco_eval
 
-assert torch.__version__.split('.')[0] == '1'
 
 print('CUDA available: {}'.format(torch.cuda.is_available()))
 
