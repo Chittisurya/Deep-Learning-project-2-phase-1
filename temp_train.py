e2489b10e16df6c0d6fc6e5cae09eaa8458d90a8
Chittisurya <145158399+Chittisurya@users.noreply.github.com>
2024-11-10 11:52:09 +0530

    Update temp_train.py

--- temp_train.py
+++ temp_train.py
@@ -1,128 +1,166 @@
 import argparse
-import collections
-import numpy as np
+import os
+import json
 import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as T
 import torch.optim as optim
-from torchvision import transforms
-from retinanet import model
-from retinanet.dataloader import CocoDataset, CSVDataset, collater, Resizer, AspectRatioBasedSampler, Augmenter, Normalizer
-from torch.utils.data import DataLoader
-from retinanet import coco_eval
-from retinanet import csv_eval
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from torch import nn
+from torchvision.models.detection import retinanet_resnet50_fpn, create_default_roi_box_predictor
 
-print('CUDA available: {}'.format(torch.cuda.is_available()))
+# Custom Dataset Class
+class CustomDataset(Dataset):
+    def __init__(self, image_dir, annotation_file, transform=None):
+        self.image_dir = image_dir
+        self.annotation_file = annotation_file
+        self.transform = transform
+        self.annotations = self.load_annotations()
 
-def main(args=None):
-    parser = argparse.ArgumentParser(description='Simple training script for training a RetinaNet network.')
+    def load_annotations(self):
+        with open(self.annotation_file) as f:
+            return json.load(f)  # Assuming COCO-style JSON format
+
+    def __len__(self):
+        return len(self.annotations['images'])
+
+    def __getitem__(self, idx):
+        img_info = self.annotations['images'][idx]
+        img_path = os.path.join(self.image_dir, img_info['file_name'])
+        image = Image.open(img_path).convert("RGB")
+        boxes = self.annotations['annotations'][idx]['bbox']
+        labels = self.annotations['annotations'][idx]['category_id']
+        
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, boxes, labels
+
+# Data augmentation and normalization pipeline
+def get_transforms():
+    return T.Compose([
+        T.ToTensor(),
+        T.Resize((800, 800)),  # Resize as needed
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # COCO mean/std
+        T.RandomHorizontalFlip(),
+        T.RandomRotation(30),
+    ])
+
+# Function to load dataset and partition it
+def partition_dataset(image_dir, annotation_file):
+    with open(annotation_file) as f:
+        annotations = json.load(f)
     
-    parser.add_argument('--dataset', help='Dataset type, must be one of csv or coco.')
-    parser.add_argument('--coco_path', help='Path to COCO directory')
-    parser.add_argument('--csv_train', help='Path to file containing training annotations (see readme)')
-    parser.add_argument('--csv_classes', help='Path to file containing class list (see readme)')
-    parser.add_argument('--csv_val', help='Path to file containing validation annotations (optional, see readme)')
-    parser.add_argument('--depth', help='Resnet depth, must be one of 18, 34, 50, 101, 152', type=int, default=50)
-    parser.add_argument('--epochs', help='Number of epochs', type=int, default=100)
-    parser.add_argument('--num_classes', type=int, default=80, help='Total number of classes, including custom classes.')
+    images = annotations['images']
+    train_images, temp_images = train_test_split(images, test_size=0.2, random_state=42)
+    val_images, test_images = train_test_split(temp_images, test_size=0.5, random_state=42)
 
-    parser = parser.parse_args(args)
+    return train_images, val_images, test_images
 
-    # Create the data loaders
-    if parser.dataset == 'coco':
-        if parser.coco_path is None:
-            raise ValueError('Must provide --coco_path when training on COCO.')
-        dataset_train = CocoDataset(parser.coco_path, set_name='train2017',
-                                    transform=transforms.Compose([Normalizer(), Augmenter(), Resizer()]))
-        dataset_val = CocoDataset(parser.coco_path, set_name='val2017',
-                                  transform=transforms.Compose([Normalizer(), Resizer()]))
+# Modify RetinaNet's output layer
+def modify_model(model, num_classes):
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = create_default_roi_box_predictor(in_features, num_classes=num_classes)
+    return model
 
-    elif parser.dataset == 'csv':
-        if parser.csv_train is None or parser.csv_classes is None:
-            raise ValueError('Must provide --csv_train and --csv_classes for CSV dataset.')
+# Training loop
+def train_one_epoch(model, train_loader, optimizer, device):
+    model.train()
+    running_loss = 0
+    for images, targets in train_loader:
+        images = [image.to(device) for image in images]
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-        dataset_train = CSVDataset(train_file=parser.csv_train, class_list=parser.csv_classes,
-                                   transform=transforms.Compose([Normalizer(), Augmenter(), Resizer()]))
-        dataset_val = None if parser.csv_val is None else CSVDataset(
-            train_file=parser.csv_val, class_list=parser.csv_classes,
-            transform=transforms.Compose([Normalizer(), Resizer()]))
-    else:
-        raise ValueError('Dataset type not understood (must be csv or coco).')
+        optimizer.zero_grad()
+        loss_dict = model(images, targets)
+        losses = sum(loss for loss in loss_dict.values())
+        losses.backward()
+        optimizer.step()
 
-    sampler = AspectRatioBasedSampler(dataset_train, batch_size=2, drop_last=False)
-    dataloader_train = DataLoader(dataset_train, num_workers=3, collate_fn=collater, batch_sampler=sampler)
+        running_loss += losses.item()
 
-    if dataset_val:
-        sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=1, drop_last=False)
-        dataloader_val = DataLoader(dataset_val, num_workers=3, collate_fn=collater, batch_sampler=sampler_val)
+    return running_loss / len(train_loader)
 
-    # Create the model
-    model_func = getattr(model, f'resnet{parser.depth}', None)
-    if not model_func:
-        raise ValueError('Unsupported model depth, must be one of 18, 34, 50, 101, 152')
+# Validation loop
+def evaluate(model, val_loader, device):
+    model.eval()
+    running_loss = 0
+    with torch.no_grad():
+        for images, targets in val_loader:
+            images = [image.to(device) for image in images]
+            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+            loss_dict = model(images, targets)
+            losses = sum(loss for loss in loss_dict.values())
+            running_loss += losses.item()
+
+    return running_loss / len(val_loader)
+
+# Visualize predictions
+def visualize_predictions(model, loader, device):
+    model.eval()
+    images, targets = next(iter(loader))
+    images = [image.to(device) for image in images]
+
+    with torch.no_grad():
+        predictions = model(images)
+
+    for i, image in enumerate(images):
+        plt.imshow(image.cpu().permute(1, 2, 0))  # Convert tensor to numpy
+        plt.title("Predictions")
+        boxes = predictions[i]['boxes'].cpu().numpy()
+        labels = predictions[i]['labels'].cpu().numpy()
+        for box, label in zip(boxes, labels):
+            plt.gca().add_patch(plt.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1], linewidth=2, edgecolor='r', facecolor='none'))
+        plt.show()
+
+# Main function to run everything
+def main(args):
+    # Load dataset and partition
+    train_images, val_images, test_images = partition_dataset(args.image_dir, args.annotation_file)
+
+    # Prepare DataLoader
+    transform = get_transforms()
+    train_dataset = CustomDataset(args.image_dir, args.annotation_file, transform=transform)
+    val_dataset = CustomDataset(args.image_dir, args.annotation_file, transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=lambda x: tuple(zip(*x)))
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=lambda x: tuple(zip(*x)))
+
+    # Load the pre-trained RetinaNet model
+    model = retinanet_resnet50_fpn(pretrained=True)
+
+    # Modify the model's output layer to account for the new classes
+    model = modify_model(model, num_classes=82)  # 80 COCO classes + 2 new classes
+
+    # Set up optimizer
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    # Move model to the appropriate device (GPU or CPU)
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model.to(device)
+
+    # Train and evaluate
+    for epoch in range(args.epochs):
+        train_loss = train_one_epoch(model, train_loader, optimizer, device)
+        val_loss = evaluate(model, val_loader, device)
+        print(f"Epoch {epoch+1}, Train Loss: {train_loss}, Validation Loss: {val_loss}")
     
-    retinanet = model_func(num_classes=parser.num_classes, pretrained=True)
-
-    if torch.cuda.is_available():
-        retinanet = torch.nn.DataParallel(retinanet).cuda()
-    
-    retinanet.train()
-    retinanet.module.freeze_bn()
-
-    optimizer = optim.Adam(retinanet.parameters(), lr=1e-5)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True)
-    loss_hist = collections.deque(maxlen=500)
-
-    print('Num training images:', len(dataset_train))
-
-    for epoch_num in range(parser.epochs):
-        retinanet.train()
-        retinanet.module.freeze_bn()
-
-        epoch_loss = []
-        for iter_num, data in enumerate(dataloader_train):
-            optimizer.zero_grad()
-            if torch.cuda.is_available():
-                classification_loss, regression_loss = retinanet([data['img'].cuda().float(), data['annot']])
-            else:
-                classification_loss, regression_loss = retinanet([data['img'].float(), data['annot']])
-                
-            loss = classification_loss.mean() + regression_loss.mean()
-            if loss == 0:
-                continue
-
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(retinanet.parameters(), 0.1)
-            optimizer.step()
-            loss_hist.append(float(loss))
-            epoch_loss.append(float(loss))
-
-            print(f"Epoch: {epoch_num} | Iter: {iter_num} | Cls loss: {classification_loss.mean():.5f} | "
-                  f"Reg loss: {regression_loss.mean():.5f} | Avg loss: {np.mean(loss_hist):.5f}")
-
-            del classification_loss, regression_loss
-
-        # Evaluation and prediction formatting
-        if parser.dataset == 'coco':
-            coco_eval.evaluate_coco(dataset_val, retinanet)
-        elif parser.dataset == 'csv' and parser.csv_val:
-            csv_eval.evaluate(dataset_val, retinanet)
-
-        scheduler.step(np.mean(epoch_loss))
-
-        # Save model
-        torch.save(retinanet.module, f'{parser.dataset}_retinanet_{epoch_num}.pt')
-
-    torch.save(retinanet, 'model_final.pt')
-
-
-# Add a helper function to format predictions
-def format_predictions(predictions):
-    formatted_predictions = {
-        'boxes': predictions[0]['boxes'].cpu().numpy(),
-        'labels': predictions[0]['labels'].cpu().numpy(),
-        'scores': predictions[0]['scores'].cpu().numpy()
-    }
-    return formatted_predictions
-
+    # Visualize predictions
+    visualize_predictions(model, train_loader, device)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Fine-tune RetinaNet on a custom dataset with two new classes.")
+    
+    # Arguments for dataset and training
+    parser.add_argument('--image_dir', type=str, required=True, help='Path to the directory containing images.')
+    parser.add_argument('--annotation_file', type=str, required=True, help='Path to the COCO-style annotation JSON file.')
+    parser.add_argument('--batch_size', type=int, default=8, help='Batch size for training and evaluation.')
+    parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate for the optimizer.')
+    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train the model.')
+
+    args = parser.parse_args()
+
+    main(args)
